ab45302b0a1574bfd017fe3655b2af7d01415faa
Josselin MONTEIL <josselin.monteil@epita.fr>
2023-10-25 22:02:56 +0200

    :zap Done all mandatory task,the last is not beautifully coded

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import datetime
 import random
 import os
 
@@ -25,6 +26,9 @@ listOfFunBanReason = ["Aucune raison",
                       "Et ça fait tik tak boom",
                       "Ca fait bim et ça fait boom",
                       "La sentence est irrévocable"]
+# Dictionary to store message counts by user
+message_counts = {}
+
 @bot.event
 async def on_ready():  # When the bot is ready
     print("I'm in")
@@ -57,6 +61,27 @@ async def on_message(message):
     if message.content.lower() == "salut tout le monde":
         author = message.author
         await message.channel.send(f"Salut tout seul, {author.mention}")
+
+    # Task 6: Activate or deactivate message flood monitoring
+    global X
+    global Y
+    global flood_active
+    global message_counts
+    if flood_active:
+        # get current author, channel and messages from author in the last Y minutes
+        author = message.author
+        channel = message.channel
+        time_threshold = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(minutes=Y)
+
+        messages = []
+        async for msg in channel.history(before=time_threshold):
+            if msg.author == author:
+                messages.append(msg)
+
+        # if the number of messages is greater than X, send a warning message
+        if len(messages) > X:
+            await channel.send(f"{author.mention} you send to much message our admistrator will be notified and can ban you")
+
     await bot.process_commands(message)
 
 # Task 4 : Give admin role to user
@@ -81,7 +106,26 @@ async def ban(ctx, member: discord.Member, reason=None):
     await member.ban(reason=reason)
     await ctx.send(f"{member.mention} has been banned for the following reason: {reason}")
 
+# Task 6: Activate or deactivate message flood monitoring
+flood_active = False
+X = 1
+Y = 1
 
+@bot.command()
+async def flood(ctx, x=None, y=None):
+    global flood_active
+    global X
+    global Y
+    if x:
+        X = x
+    if y:
+        Y = y
+
+    flood_active = not flood_active
+    if flood_active:
+        await ctx.send(f"Flood monitoring is now active with the following parameters; X={X} and Y={Y}")
+    else:
+        await ctx.send("Flood monitoring is now deactivated.")
 
 print(DISCORD_TOKEN)
 token = DISCORD_TOKEN
